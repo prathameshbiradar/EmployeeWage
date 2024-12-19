1584d619dfc777c06d409fcd4b3f979a35adf617
prathmeshbiradar <psbiradar948@gmail.com>
2024-12-19 16:44:58 +0530

    commited UC3

--- src/co/employeewage/Main.java
+++ src/co/employeewage/Main.java
@@ -7,16 +7,34 @@ public class Main {
         System.out.println("Welcome to Employee Wage");
 
         Random random = new Random();
+        // Wage per Hour
         int wagePerHour = 20;
-        int dailyHours = 8;
-        int dailyWage = wagePerHour * dailyHours;
-        int attendence = random.nextInt(2);
 
+        // full-time and part-time Hours
+        int fullTimeHours = 8;
+        int partTimeHours = 8;
+
+        // Calculate daily wages for full-time and part-time hours
+        int fullTimeWage = wagePerHour * fullTimeHours;
+        int partTimeWage = wagePerHour * partTimeHours;
+
+        int attendence = random.nextInt(3); //generate 0,1,2
+
+        // Full-Time Employee
         if(attendence==1)
         {
             System.out.println("Employee is Present");
-            System.out.println("Daily wage is $"+dailyWage);
+            System.out.println("Employee Type: Full-Time");
+            System.out.println("Daily wage is $"+fullTimeWage);
         }
+        // Part-Time Employee
+        else if (attendence == 2) {
+            System.out.println("Employee is present");
+            System.out.println("Employee Type: Full-Time");
+            System.out.println("Daily wage is $"+partTimeWage);
+
+        }
+        // Employee is absent
         else {
             System.out.println("Employee is absent");
             System.out.println("Daily wage is $0");
