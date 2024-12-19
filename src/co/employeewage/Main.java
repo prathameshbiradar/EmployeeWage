16f24e59d60e4adc9fa37eaa13b036ff0c222e77
prathmeshbiradar <psbiradar948@gmail.com>
2024-12-19 16:37:04 +0530

    commited UC2

--- src/co/employeewage/Main.java
+++ src/co/employeewage/Main.java
@@ -7,14 +7,19 @@ public class Main {
         System.out.println("Welcome to Employee Wage");
 
         Random random = new Random();
+        int wagePerHour = 20;
+        int dailyHours = 8;
+        int dailyWage = wagePerHour * dailyHours;
         int attendence = random.nextInt(2);
 
         if(attendence==1)
         {
             System.out.println("Employee is Present");
+            System.out.println("Daily wage is $"+dailyWage);
         }
         else {
             System.out.println("Employee is absent");
+            System.out.println("Daily wage is $0");
         }
 
     }
