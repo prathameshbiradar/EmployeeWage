57c2003a01ae325cf0e145bd0b8fc6ffacd0427f
prathmeshbiradar <psbiradar948@gmail.com>
2024-12-19 16:27:54 +0530

    Commited initial program

---
+++ src/co/employeewage/Main.java
@@ -0,0 +1,10 @@
+package co.employeewage;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage");
+
+        }
+    }
