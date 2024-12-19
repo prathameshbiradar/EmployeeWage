d8c60ffbebc87e4d2cd004ef4af4061bee31163c
prathmeshbiradar <psbiradar948@gmail.com>
2024-12-19 16:56:59 +0530

    Commited UC5

--- src/co/employeewage/Main.java
+++ src/co/employeewage/Main.java
@@ -14,9 +14,18 @@ public class Main {
         int fullTimeHours = 8;
         int partTimeHours = 8;
 
+        // Working days in a month
+        int workingDaysPerMonth = 20;
+
         // Calculate daily wages for full-time and part-time hours
-        int fullTimeWage = wagePerHour * fullTimeHours;
-        int partTimeWage = wagePerHour * partTimeHours;
+        int fullTimeDailyWage = wagePerHour * fullTimeHours;
+        int partTimeDailyWage = wagePerHour * partTimeHours;
+
+        //  monthly wage for full-time and part-time employees
+        int fullTimeMonthlyWage = fullTimeDailyWage * workingDaysPerMonth;
+        int partTimeMonthlyWage = partTimeDailyWage * workingDaysPerMonth;
+
+
 
         int attendence = random.nextInt(3); //generate 0,1,2
 
@@ -26,15 +35,17 @@ public class Main {
             case 1:
                 System.out.println("Employee is Present");
                 System.out.println("Employee Type: Full-Time");
-                System.out.println("Daily wage is $"+fullTimeWage);
+                System.out.println("Daily wage is $"+fullTimeDailyWage);
+                System.out.println("Monthly Wage: $" + fullTimeMonthlyWage);
                 break;
             case 2:
                 // Part-Time Employee
                 System.out.println("Employee is present");
                 System.out.println("Employee Type: Full-Time");
-                System.out.println("Daily wage is $"+partTimeWage);
+                System.out.println("Daily wage is $"+partTimeDailyWage);
+                System.out.println("Monthly Wage: $" + partTimeMonthlyWage);
                 break;
-            case 3:
+            default:
                 // Employee is absent
                 System.out.println("Employee is absent");
                 System.out.println("Daily wage is $0");
