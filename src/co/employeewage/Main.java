da4b85e82dceb7b81bbad7e6df94548197a32105
prathmeshbiradar <psbiradar948@gmail.com>
2024-12-19 16:32:45 +0530

    commited UC1

--- src/co/employeewage/Main.java
+++ src/co/employeewage/Main.java
@@ -6,5 +6,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
 
+        Random random = new Random();
+        int attendence = random.nextInt(2);
+
+        if(attendence==1)
+        {
+            System.out.println("Employee is Present");
         }
+        else {
+            System.out.println("Employee is absent");
+        }
+
     }
+}
